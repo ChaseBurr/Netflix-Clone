0ef651f8b7fec8e42fa81924a4a6fccf1ec9acb1
Chase <chase@chaseburr.com>
2021-01-20 21:26:16 -0700

    updated app styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
+
+// CSS imports
+import "./App.css";
+import "./Pages/PageStyling.css";
+import "./Components/ComponentStyling.css";
+
 function App() {
-   return <div className="Project"></div>;
+   return (
+      <div className="Project">
+         <Router>
+            <Switch>
+               <Route path="/" component={LandingPage} />
+            </Switch>
+         </Router>
+      </div>
+   );
 }
 
 export default App;
