1da1ba997a5d56a8aa8c1fad638a827a74f2888a
Chase <chase@chaseburr.com>
2021-01-21 19:09:59 -0700

    updated browse page

--- src/Pages/Browse.js
+++ src/Pages/Browse.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MovieCard from "../Components/Cards/MovieCard";
+import ShowList from "../Components/ShowList/ShowList";
+import { getPopular } from "../Components/FetchData";
 
 function Browse() {
+   const [popularData, setPopularData] = useState([]);
+
+   useEffect(() => {
+      async function getData() {
+         setPopularData(
+            await getPopular().then((response) => response.data.results)
+         );
+      }
+      getData();
+   }, []);
+
    return (
-      <div>
-         <div className="background-film">
-            <div className="overlay"></div>
+      <>
+         <div className="browser-hero">
+            <div className="background-film"></div>
             <div className="browser-navbar">
                <img
                   src="/Netflix-Clone/images/netflix-logo-1.png"
@@ -12,21 +27,41 @@ function Browse() {
                   alt=""
                />
                <ul className="show-tabs">
-                  <li>Home</li>
-                  <li>TV Shows</li>
-                  <li>Movies</li>
-                  <li>New & Popular</li>
-                  <li>My List</li>
+                  <li>
+                     <Link to="/browse">Home</Link>
+                  </li>
+                  <li>
+                     <Link to="/browse/tv">TV Shows</Link>
+                  </li>
+                  <li>
+                     <Link to="/browse/movies">Movies</Link>
+                  </li>
+                  <li>
+                     <Link to="popular">New & Popular</Link>
+                  </li>
+                  <li>
+                     <Link>My List</Link>
+                  </li>
                </ul>
                <ul className="personal-tabs">
                   <li>
                      <i className="fas fa-search"></i>
                   </li>
-                  <li>KIDS</li>
-                  <li>DVD</li>
-                  <li>present</li>
-                  <li>notification</li>
-                  <li>profile</li>
+                  <li>
+                     <Link to="/browse/kids">KIDS</Link>
+                  </li>
+                  <li>
+                     <Link to="/browse/dvd">DVD</Link>
+                  </li>
+                  <li>
+                     <Link to="/">present</Link>
+                  </li>
+                  <li>
+                     <Link to="/">notification</Link>
+                  </li>
+                  <li>
+                     <Link to="/">profile</Link>
+                  </li>
                </ul>
             </div>
             <div className="film">
@@ -41,7 +76,24 @@ function Browse() {
                </div>
             </div>
          </div>
-      </div>
+         <div className="show-list-section">
+            <ShowList header="Popular">
+               {popularData ? (
+                  popularData.map((movie, index) => {
+                     if (movie.backdrop_path)
+                        return (
+                           <MovieCard
+                              imgSrc={movie.backdrop_path}
+                              key={index}
+                           />
+                        );
+                  })
+               ) : (
+                  <div>Loading...</div>
+               )}
+            </ShowList>
+         </div>
+      </>
    );
 }
 
