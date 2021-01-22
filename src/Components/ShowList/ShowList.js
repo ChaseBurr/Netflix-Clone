f6d7750cc077ba1b7da39eb8427f9c64659fdbc1
Chase <chase@chaseburr.com>
2021-01-21 20:37:19 -0700

    added glider css

--- src/Components/ShowList/ShowList.js
+++ src/Components/ShowList/ShowList.js
@@ -3,15 +3,22 @@ import React from "react";
 function ShowList(props) {
    return (
       <div className="movie-list">
-         <h1>{props.header}</h1>
-         <button className="glider-prev">
-            <i className="fa fa-chevron-left"></i>
-         </button>
-         <div className="shows glider">{props.children}</div>
-         <div id="dots" className="glider-dots"></div>
-         <button className="glider-next">
-            <i className="fa fa-chevron-right"></i>
-         </button>
+         <div className="glider-contain multiple">
+            <h1>{props.header}</h1>
+            <div className="shows glider">{props.children}</div>
+            <button
+               aria-label="Previous"
+               className="glider-prev glider-arrow-left"
+            >
+               <i className="fa fa-chevron-left "></i>
+            </button>
+            <button
+               aria-label="Next"
+               className="glider-next glider-arrow-right"
+            >
+               <i className="fa fa-chevron-right"></i>
+            </button>
+         </div>
       </div>
    );
 }
