4a9c39a19bd68eac1cf054e5b12ab9b3bc9c1385
Chase <chase@chaseburr.com>
2021-01-27 21:43:36 -0700

    updated home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+// React Imports
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-
-// Pages
-import LandingPage from "./Pages/LandingPage";
-import Login from "./Pages/Login";
-import Browse from "./Pages/Browse";
 
 // CSS imports
 import "./App.css";
-import "./Pages/PageStyling.css";
-import "./Components/ComponentStyling.css";
 
+// Component imports
+import { Row, Banner, Navbar } from "./Components/exports";
+
+// request import
+import requests from "./requests";
+
+// App Component
 function App() {
    return (
-      <div className="Project">
-         <Router>
-            <Switch>
-               <Route exact path="/" component={LandingPage} />
-               <Route exact path="/login" component={Login} />
-               <Route exact path="/browse" component={Browse} />
-            </Switch>
-         </Router>
+      <div className="app">
+         {/* Nav */}
+         <Navbar />
+
+         {/* Banner */}
+         <Banner />
+
+         {/* Rows */}
+         <Row
+            title="NETFLIX ORIGINALS"
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+         />
+         <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+         <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       </div>
    );
 }
